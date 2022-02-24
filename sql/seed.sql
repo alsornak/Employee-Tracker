bfbcf6bd9b4e4a1f121c31b0c450f83b8630bd34
alsornak <al.sorna01@gmail.com>
2022-02-24 22:13:04 +1100

    modified the files

--- sql/seed.sql
+++ sql/seed.sql
@@ -24,12 +24,12 @@ VALUES('Account Executive', 100000, 1),
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Dottie', 'O''Neil', 12, NULL),
+('Harry', 'O''Neil', 12, NULL),
 ('Becky', 'Houlihan', 13, 1),
 ('April', 'Romper', 14, 1),
-('Dale', 'Robson', 3, 2),
+('Dale', 'Robort', 3, 2),
 ('William', 'Louie', 9, 2),
-('Carl', 'Cliffbeard', 11, 2),
+('Eric', 'Cliffbeard', 11, 2),
 ('Jackie', 'O''Rourke', 6, 2),
 ('Bob', 'Johnson', 1, 4),
 ('Frank', 'Dodson', 1, 4),
@@ -39,5 +39,5 @@ VALUES
 ('Dom', 'Chewer', 5, 7),
 ('Mary', 'Bronson', 7, 5),
 ('Sarah', 'Robbie', 8, 5),
-('Jeb', 'Johnnygriff', 10, 6),
-('Barton', 'Heathcliffscruff', 10, 6);
+('Jeba', 'Johnnygriff', 10, 6),
+('Barton', 'Henry', 10, 6);
